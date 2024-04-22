081f85f24fb505b290576de1a5305e5e71dccb07
nrsimonelli <nrsimonelli@gmail.com>
2024-04-21 22:00:10 -0500

    added pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+
+export default function About() {
+  return (
+    <div className='max-w-7xl mx-auto shadow-lg -mt-20 z-10 bg-background rounded-md p-6 space-y-4'>
+      {/* h1 */}
+      <p className='text-3xl font-bold text-primary'>About</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        voluptates molestiae voluptas vero laudantium. Minus, excepturi
+        temporibus! Molestias, pariatur omnis id illo totam deleniti, molestiae
+        vitae suscipit mollitia sit explicabo!
+      </p>
+      {/* h2 */}
+      <p className='text-2xl font-bold text-primary'>
+        What is Tournament Rating?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        praesentium est commodi nemo? Repudiandae obcaecati similique cumque
+        veritatis exercitationem laboriosam aliquam, dignissimos culpa in itaque
+        sed esse quia iusto omnis?
+      </p>
+      <p className='text-2xl text-primary font-bold'>
+        What is Tournament Rating?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        praesentium est commodi nemo? Repudiandae obcaecati similique cumque
+        veritatis exercitationem laboriosam aliquam, dignissimos culpa in itaque
+        sed esse quia iusto omnis?
+      </p>
+    </div>
+  )
+}
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,8 +1,52 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 import { createClient } from '@/lib/supabase/server'
+import { Json } from '@/types/supabase'
 
-export default async function Table() {
+export default async function Leaderboard() {
   const supabase = createClient()
-  const { data: players } = await supabase.from('players').select()
+  const { data: players } = await supabase
+    .from('players')
+    .select()
+    .order('current_rating->ordinal', { ascending: false })
 
-  return <pre>{JSON.stringify(players, null, 2)}</pre>
+  const hasValidOrdinal = (rating: Json) => {
+    return (
+      typeof rating === 'object' &&
+      typeof (rating as { ordinal: number }).ordinal === 'number'
+    )
+  }
+
+  return (
+    <div className='max-w-7xl mx-auto shadow-lg -mt-20 z-10 bg-background rounded-md p-6'>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Rank</TableHead>
+            <TableHead>Player</TableHead>
+            <TableHead>Rating</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {players?.map((player, index) => {
+            const ordinal = (player.current_rating as { ordinal: number })
+              .ordinal
+            return (
+              <TableRow key={player.id}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{player.username}</TableCell>
+                <TableCell>{Math.round(ordinal)}</TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
